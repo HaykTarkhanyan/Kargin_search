f50375fbcbb519b64a7c3445b412f40eac35040e
aprikyan <haykaprikyan@outlook.com>
2021-12-04 21:30:44 +0400

    Refactored `KarginSearch` to initialize only once

--- Kargin_project/KarginSearch.py
+++ Kargin_project/KarginSearch.py
@@ -3,30 +3,28 @@ from fuzzywuzzy import fuzz
 
 
 class KarginSearch:
-    def __init__(self, query, filters, data):
-        self.query = query
-        self.filters = filters
+    def __init__(self, data):
         self.data = data
 
-    def apply_filters(self):
+    def apply_filters(self, filters):
         mask = pd.Series([True] * len(self.data))
 
-        if "Հիմնական դերասաններ" in self.filters:
-            mask &= self.data["Հիմնական դերասաններ"].apply(lambda x: set(self.filters["Հիմնական դերասաններ"]) <= x)
-        if "հիմնական դերասանների քանակ" in self.filters:
-            mask &= self.data["հիմնական դերասանների քանակ"] == self.filters["հիմնական դերասանների քանակ"]
-        if "Դերերի անուններ" in self.filters:
-            mask &= self.data["Դերերի անուններ"].apply(lambda x: set(self.filters["Դերերի անուններ"]) <= x)
-        if "Վայր" in self.filters:
-            mask &= self.data["Վայր"] == self.filters["Վայր"]
-        if "Լուսավորվածություն" in self.filters:
-            mask &= self.data["Լուսավորվածություն"] == self.filters["Լուսավորվածություն"]
-        if "Լեզուներ" in self.filters:
-            mask &= self.data["Լեզուներ"] == self.filters["Լեզուներ"]
+        if "Հիմնական դերասաններ" in filters:
+            mask &= self.data["Հիմնական դերասաններ"].apply(lambda x: set(filters["Հիմնական դերասաններ"]) <= x)
+        if "հիմնական դերասանների քանակ" in filters:
+            mask &= self.data["հիմնական դերասանների քանակ"] == filters["հիմնական դերասանների քանակ"]
+        if "Դերերի անուններ" in filters:
+            mask &= self.data["Դերերի անուններ"].apply(lambda x: set(filters["Դերերի անուններ"]) <= x)
+        if "Վայր" in filters:
+            mask &= self.data["Վայր"] == filters["Վայր"]
+        if "Լուսավորվածություն" in filters:
+            mask &= self.data["Լուսավորվածություն"] == filters["Լուսավորվածություն"]
+        if "Լեզուներ" in filters:
+            mask &= self.data["Լեզուներ"] == filters["Լեզուներ"]
 
         return self.data[mask]
 
-    def exact_search(self):
+    def exact_search(self, query, data):
         # debug purposes; remove later
         # self.data = pd.read_csv("../Կարգին 2+3րդ փլեյլիստ - Sheet3.csv").dropna(subset=["Տեքստ"])
 
@@ -36,11 +34,11 @@ class KarginSearch:
         # TODO:
         # change "Տեքստ" to something less meaningless
 
-        mask = self.data["Տեքստ"].str.contains(self.query)
+        mask = data["Տեքստ"].str.contains(query)
 
         # change this behavior if needed
         # (hint: don't)
-        return self.data[mask] if mask.any() else None
+        return data[mask] if mask.any() else None
 
     @staticmethod
     def get_similarity_score(keyword_base, keyword_input):
@@ -64,15 +62,14 @@ class KarginSearch:
         score = 0.3 * a + 0.3 * b + 0.4 * c
         return score
 
-    def fuzzy_search(self):
+    def fuzzy_search(self, query, data):
         # TODO:
         # replace "Տեքստ" with sth more meaningful
-        self.data["similarity_score"] = self.data["Տեքստ"].apply(KarginSearch.get_similarity_score,
-                                                                 keyword_input=self.query)
-        self.data = self.data.sort_values("similarity_score", ascending=False)
-        return self.data
+        data["similarity_score"] = data["Տեքստ"].apply(KarginSearch.get_similarity_score, keyword_input=query)
+        data = data.sort_values("similarity_score", ascending=False)
+        return data
 
-    def find(self):
-        self.data = self.apply_filters()
-        results = self.exact_search() or self.fuzzy_search()
+    def find(self, query, filters):
+        data = self.apply_filters(filters)
+        results = self.exact_search(data, query) or self.fuzzy_search(data, query)
         return results
